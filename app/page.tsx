da830b2fc5cc10932efb495a4ea17be1c89ff3e9
towndown <tocka992@gmail.com>
2025-03-23 19:45:08 +0900

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
 import styles from '../app/styles/Home.module.css';
-interface Post{
-    id: number;
-    title: string;
-    content: string;
-}
-export default async function Home(){
-    const response = await fetch('http://localhost:3000/api/posts');
-    const posts = await response.json();
-    return(
-        <div>
-            <div className={styles.container}></div>
-                <h1>Hello</h1>
-            <button className={styles.buton}>Узнать больше</button>
-            <ul>
-                {posts.map((post: Post) => (
-                    <li key={post.id}>
-                        <a href={`/posts/${post.id}`}>{post.title}</a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+export default function Home(){
+	return(
+		<div>
+			<div className={styles.container}></div>
+				<h1>Hello</h1>
+			<button className={styles.button}>Узнать больше</button>
+		</div>
+	);
 }

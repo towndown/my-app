9bf0c18886b65262860558a1d0fde36f3f985043
towndown <tocka992@gmail.com>
2025-03-23 19:29:18 +0900

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import styles from '../styles/Home.module.css';
 export default function Home(){
     return(
-        <div className="{styles.container}">
+        <div className={styles.container}>
             <h1>Hello</h1>
         
-        <button className='{styles.button}s'>Узнать больше</button>
+        <button className={styles.button}>Узнать больше</button>
         </div>
     );
 }
